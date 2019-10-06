9166499f4b8098da2f6ed36b41e94bfe5fc94951
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2019-10-06 22:04:15 +0200

    indented code

--- AsyncECS/TupleHelper.hpp
+++ AsyncECS/TupleHelper.hpp
@@ -24,44 +24,44 @@ namespace TupleHelper {
     }
     
     template <class Haystack, class Needle>
-struct contains;
+    struct contains;
 
-template <class Car, class... Cdr, class Needle>
-struct contains<std::tuple<Car, Cdr...>, Needle> : contains<std::tuple<Cdr...>, Needle>
-{};
+    template <class Car, class... Cdr, class Needle>
+    struct contains<std::tuple<Car, Cdr...>, Needle> : contains<std::tuple<Cdr...>, Needle>
+    {};
 
-template <class... Cdr, class Needle>
-struct contains<std::tuple<Needle, Cdr...>, Needle> : std::true_type
-{};
+    template <class... Cdr, class Needle>
+    struct contains<std::tuple<Needle, Cdr...>, Needle> : std::true_type
+    {};
 
-template <class Needle>
-struct contains<std::tuple<>, Needle> : std::false_type
-{};
-
-
-
-template <class Out, class In>
-struct filter;
-
-template <class... Out, class InCar, class... InCdr>
-struct filter<std::tuple<Out...>, std::tuple<InCar, InCdr...>>
-{
-  using type = typename std::conditional<
-    contains<std::tuple<Out...>, InCar>::value
-    , typename filter<std::tuple<Out...>, std::tuple<InCdr...>>::type
-    , typename filter<std::tuple<Out..., InCar>, std::tuple<InCdr...>>::type
-  >::type;
-};
-
-template <class Out>
-struct filter<Out, std::tuple<>>
-{
-  using type = Out;
-};
+    template <class Needle>
+    struct contains<std::tuple<>, Needle> : std::false_type
+    {};
 
 
-template <class T>
-using without_duplicates = typename filter<std::tuple<>, T>::type;
+
+    template <class Out, class In>
+    struct filter;
+
+    template <class... Out, class InCar, class... InCdr>
+    struct filter<std::tuple<Out...>, std::tuple<InCar, InCdr...>>
+    {
+      using type = typename std::conditional<
+        contains<std::tuple<Out...>, InCar>::value
+        , typename filter<std::tuple<Out...>, std::tuple<InCdr...>>::type
+        , typename filter<std::tuple<Out..., InCar>, std::tuple<InCdr...>>::type
+      >::type;
+    };
+
+    template <class Out>
+    struct filter<Out, std::tuple<>>
+    {
+      using type = Out;
+    };
+
+
+    template <class T>
+    using without_duplicates = typename filter<std::tuple<>, T>::type;
     
     
 }
