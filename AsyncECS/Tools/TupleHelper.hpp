c67479b7efb16f3febf6e61ba5787ebb7e2d75fa
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2019-10-31 21:58:18 +0100

    added has_type

--- AsyncECS/Tools/TupleHelper.hpp
+++ AsyncECS/Tools/TupleHelper.hpp
@@ -79,5 +79,11 @@ namespace TupleHelper {
     };
     
     
+    template <typename T, typename Tuple>
+    struct has_type;
+
+    template <typename T, typename... Us>
+    struct has_type<T, std::tuple<Us...>> : std::disjunction<std::is_same<T, Us>...> {};
+        
 }
 }
