1c212e696c813672878df03147fc41cc57c6e208
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2019-11-01 23:32:17 +0100

    Added simple example

---
+++ AsyncECS/Simple.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "Registry.hpp"
+#include "Scene.hpp"
+
+using namespace AsyncECS;
+
+struct Position {
+    float x;
+    float y;
+};
+
+struct Velocity {
+    float x;
+    float y;
+};
+
+struct MovementSystem : System<Position, const Velocity>, NoDependencies {
+    void Initialize() { }
+    void Update(Position& position, const Velocity& velocity) {
+        position.x += velocity.x;
+        position.y += velocity.y;
+    }
+};
+
+int main() {
+    using Components = ComponentTypes<Position, Velocity>;
+    using Systems = SystemTypes<MovementSystem>;
+    using RegistryType = Registry<Components>;
+    
+    RegistryType registry;
+    Scene<RegistryType, Systems> scene(registry);
+    
+    auto gameObject = scene.CreateGameObject();
+    scene.AddComponent<Position>(gameObject);
+    scene.AddComponent<Velocity>(gameObject, 1.0f, 2.0f);
+    
+    scene.Update();
+    
+    auto& p = scene.GetComponent<Position>(gameObject);
+    std::cout << "Position " << p.x << " " << p.y << std::endl;
+    
+    return 0;
+}
