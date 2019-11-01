5fd1e2ae08a94f02f8757a92188d7582bb8bcbf8
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2019-11-01 19:18:03 +0100

    now measuring in microseconds

--- AsyncECS/Tools/Timer.cpp
+++ AsyncECS/Tools/Timer.cpp
@@ -16,7 +16,7 @@ Timer::Timer() {
 int Timer::Stop() {
     std::chrono::steady_clock::time_point endTime = std::chrono::steady_clock::now();
     
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
 //    std::cout << name <<" : " << duration << std::endl;
     return static_cast<int>(duration);
 }
