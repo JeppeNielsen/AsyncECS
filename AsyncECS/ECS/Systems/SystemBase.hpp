27c6db3c1e16ffbd9e4bd1e253acc6e4c4c05ef2
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2020-06-05 22:57:12 +0200

    all methods in SystemBase made constexpr

--- AsyncECS/ECS/Systems/SystemBase.hpp
+++ AsyncECS/ECS/Systems/SystemBase.hpp
@@ -24,57 +24,57 @@ template<typename...T>
 struct SystemBase {
 
     template<typename O>
-    std::tuple<O*> GetComponentType(const O* ptr) const {
+    constexpr std::tuple<O*> GetComponentType(const O* ptr) const {
         return std::make_tuple((O*)nullptr);
     }
     
     template<typename O>
-    std::tuple<O*> GetComponentType(O* ptr) const {
+    constexpr std::tuple<O*> GetComponentType(O* ptr) const {
         return std::make_tuple((O*)1);
     }
 
-    std::tuple<T*...> GetComponentTypes() {
+    constexpr std::tuple<T*...> GetComponentTypes() {
         return std::tuple_cat(GetComponentType((T*)nullptr)...);
     }
     
     template<typename ComponentType>
-    bool HasComponentType() {
+    constexpr bool HasComponentType() {
         return TupleHelper::HasType<ComponentType, std::tuple<T...> >::value;
     }
 
     template<typename O, typename Components>
-    const O& Get(const GameObject gameObject, Components& components, const O* ptr) const {
+    constexpr const O& Get(const GameObject gameObject, Components& components, const O* ptr) const {
         return (const O&)std::get<ComponentContainer<std::remove_const_t<O>>>(components).GetConst(gameObject);
     }
     
     template<typename O, typename Components>
-    O& Get(const GameObject gameObject, Components& components, O* ptr) const {
+    constexpr O& Get(const GameObject gameObject, Components& components, O* ptr) const {
         return (O&)((ComponentContainer<std::remove_const_t<O>>&)std::get<ComponentContainer<std::remove_const_t<O>>>(components)).GetNoChange(gameObject);
     }
    
     template<typename Components>
-    std::tuple<T&...> GetComponentValuesFromGameObject(const GameObject gameObject, Components& components) const {
+    constexpr std::tuple<T&...> GetComponentValuesFromGameObject(const GameObject gameObject, Components& components) const {
         return std::tie(Get(gameObject, components, (T*)nullptr)...);
     }
     
     template<typename O, typename Components>
-    void ChangeComponent(const GameObject gameObject, Components& components, const O* ptr) const {
+    constexpr void ChangeComponent(const GameObject gameObject, Components& components, const O* ptr) const {
         /*empty*/ // no change for const components
     }
     
     template<typename O, typename Components>
-    void ChangeComponent(const GameObject gameObject, Components& components, O* ptr) const {
+    constexpr void ChangeComponent(const GameObject gameObject, Components& components, O* ptr) const {
         ((ComponentContainer<std::remove_const_t<O>>&)std::get<ComponentContainer<std::remove_const_t<O>>>(components)).SetChanged(gameObject);
     }
     
     template<typename Components>
-    void ChangeComponents(const GameObject gameObject, Components& components) const {
+    constexpr void ChangeComponents(const GameObject gameObject, Components& components) const {
         using expander = int[];
         (void) expander { 0, ((void)ChangeComponent(gameObject, components, (T*)nullptr),0)... };
     }
     
     template<typename Components, typename ComponentObjects>
-    GameObjectCollection& GetObjects(ComponentObjects& componentObjects) const {
+    constexpr GameObjectCollection& GetObjects(ComponentObjects& componentObjects) const {
         const int numElements = sizeof...(T);
         
         int indicies[] { TupleHelper::Index<ComponentContainer<std::remove_const_t<std::remove_pointer_t<T>>>, Components>::value... };
@@ -93,7 +93,7 @@ struct SystemBase {
     }
     
     template<typename Components>
-    const GameObjectCollection::Objects& GetChangedObjects(const Components& components) const {
+    constexpr const GameObjectCollection::Objects& GetChangedObjects(const Components& components) const {
         const int numElements = sizeof...(T);
         
         const GameObjectCollection::Objects* objects[] { &std::get<ComponentContainer<std::remove_const_t<T>>>(components).changedThisFrame.objects... };
