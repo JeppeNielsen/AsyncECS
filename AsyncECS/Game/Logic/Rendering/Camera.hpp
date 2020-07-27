9bcb5398c1bad97bc9be5a67b1757d05da89a6b9
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2020-07-27 23:28:25 +0200

    render tweaks

---
+++ AsyncECS/Game/Logic/Rendering/Camera.hpp
@@ -0,0 +1,16 @@
+//
+//  Camera.hpp
+//  AsyncECS
+//
+//  Created by Jeppe Nielsen on 27/07/2020.
+//  Copyright © 2020 Jeppe Nielsen. All rights reserved.
+//
+
+#pragma once
+#include "Vector2.hpp"
+
+namespace Game {
+struct Camera {
+    Vector2 ViewSize;
+};
+}
---
+++ AsyncECS/Game/Logic/Rendering/Tests/RenderSystemTests.hpp
@@ -0,0 +1,16 @@
+//
+//  RenderSystemTests.hpp
+//  AsyncECS
+//
+//  Created by Jeppe Nielsen on 27/07/2020.
+//  Copyright © 2020 Jeppe Nielsen. All rights reserved.
+//
+
+#pragma once
+#include "TestBase.hpp"
+
+namespace Game::Tests {
+    struct RenderSystemTests : ::Tests::TestBase {
+        void Run();
+    };
+}
