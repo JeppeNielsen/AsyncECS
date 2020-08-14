15e66b1d039cf049092a1771960ce88ac36dd723
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2020-08-14 22:13:23 +0200

    added Core/Platform/Rendering and tools folders

---
+++ AsyncECS/Game/Core/Engine.cpp
@@ -0,0 +1,48 @@
+#include "Engine.hpp"
+#include "Timer.hpp"
+#include "Window.hpp"
+
+using namespace Game;
+
+void Engine::MainLoop() {
+
+    Screen::MainScreen = &state->device.Screen;
+    
+    Window window;
+    Timer timer;
+    
+    float elapsed = 1.0f / 60.0f;
+    
+    Window::MainLoopData data;
+    data.Initialize =
+    [&, this]() {
+        state->Initialize();
+        timer.Start();
+    };
+    
+    data.Update =
+    [&, this]() {
+        elapsed = timer.Stop();
+        timer.Start();
+        window.inputDevice.StartFrame();
+        window.inputDevice.UpdateInputManager(state->device.Input);
+        state->Update(elapsed);
+        window.inputDevice.EndFrame();
+        window.PreRender();
+        state->Render();
+        window.PostRender();
+        return state->device.exited;
+    };
+    
+    data.ScreenSize =
+    [&, this] (int width, int height) {
+        state->device.Screen.Size = Vector2(width, height);
+    };
+    
+    data.ScreenScalingFactor =
+    [&, this] (float scalingFactor) {
+        state->device.Screen.ScalingFactor = scalingFactor;
+    };
+    
+    window.StartLoop(data);
+}
