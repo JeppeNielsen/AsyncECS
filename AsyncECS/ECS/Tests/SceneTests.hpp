f6161f902bc8e866c6a0a2e5b3ccc47b0585b66c
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2020-08-27 23:49:44 +0200

    Added Scene tests

---
+++ AsyncECS/ECS/Tests/SceneTests.hpp
@@ -0,0 +1,16 @@
+//
+//  SceneTests.hpp
+//  AsyncECS
+//
+//  Created by Jeppe Nielsen on 27/08/2020.
+//  Copyright © 2020 Jeppe Nielsen. All rights reserved.
+//
+
+#pragma once
+#include "TestBase.hpp"
+
+namespace AsyncECS::Tests {
+    struct SceneTests : ::Tests::TestBase {
+        void Run();
+    };
+}
